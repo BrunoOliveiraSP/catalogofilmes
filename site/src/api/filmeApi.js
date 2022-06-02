ea79fc37a36c157af20245d543fa97862a98ea0c
Bruno de Oliveira <brunodeoliveira.22.10@gmail.com>
2022-06-01 21:31:23 -0300

    cadastrar parte 1

---
+++ site/src/api/filmeApi.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+const api = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+
+export async function cadastrarFilme(nome, avaliacao, lancamento, disponivel, sinopse, usuario) {
+    const resposta = await api.post('/filme', {
+        nome: nome,
+        sinopse: sinopse,
+        avaliacao: avaliacao,
+        disponivel: disponivel,
+        lancamento: lancamento,
+        usuario: usuario
+    })
+    return resposta.data;
+}
+
+
+export async function enviarImagemFilme(id, imagem) {
+    const formData = new FormData();
+    formData.append('capa', imagem);
+
+    const resposta = await api.put(`/filme/${id}/capa`, formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+    });
+    
+    return resposta.status;
+}
--- site/src/pages/cadastrar/index.js
+++ site/src/pages/cadastrar/index.js
@@ -1,10 +1,41 @@
 import Menu from '../../components/menu'
 import Cabecalho from '../../components/cabecalho'
 
+import { cadastrarFilme, enviarImagemFilme } from '../../api/filmeApi'
+import storage from 'local-storage'
+
 import './index.scss'
+import { useState } from 'react'
+
+import { toast } from 'react-toastify';
+
+
+
+
 
 
 export default function Index() {
+    const [nome, setNome] = useState('');
+    const [sinopse, setSinopse] = useState('');
+    const [avaliacao, setAvaliacao] = useState(0);
+    const [disponivel, setDisponivel] = useState(false);
+    const [lancamento, setLancamento] = useState('');
+    const [imagem, setImagem] = useState('');
+
+
+
+    async function salvarClick() {
+        try {
+            const usuario = storage('usuario-logado').id;
+            const r = await cadastrarFilme(nome, avaliacao, lancamento, disponivel, sinopse, usuario);
+
+            toast.dark('🚀 Filme cadastrado com sucesso!');
+        } catch (err) {
+            toast.error(err.response.data.erro);
+        }
+    }
+
+
     return (
         <main className='page page-cadastrar'>
             <Menu selecionado='cadastrar' />
@@ -24,33 +55,33 @@ export default function Index() {
                             <div>
                                 <div className='form-row'>
                                     <label>Nome:</label>
-                                    <input type='text' placeholder='Nome do filme' />
+                                    <input type='text' placeholder='Nome do filme' value={nome} onChange={e => setNome(e.target.value)} />
                                 </div>
                                 <div className='form-row'>
                                     <label>Avaliação:</label>
-                                    <input type='number' placeholder='0' />
+                                    <input type='number' placeholder='0' value={avaliacao} onChange={e => setAvaliacao(e.target.value)} />
                                 </div>
                                 <div className='form-row'>
                                     <label>Lançamento:</label>
-                                    <input type='date' />
+                                    <input type='date' value={lancamento} onChange={e => setLancamento(e.target.value)} />
                                 </div>
                                 <br />
                                 <div className='form-row'>
                                     <label></label>
-                                    <input type='checkbox' /> &nbsp; Disponível
+                                    <input type='checkbox' checked={disponivel} onChange={e => setDisponivel(e.target.checked)} /> &nbsp; Disponível
                                 </div>
                             </div>
                             <div>
                                 <div className='form-row' style={{alignItems: 'flex-start'}}>
                                     <label style={{marginTop: '13px'}}>Sinopse:</label>
-                                    <textarea placeholder='Sinopse do filme' />
+                                    <textarea placeholder='Sinopse do filme' value={sinopse} onChange={e => setSinopse(e.target.value)} />
                                 </div>
                                 <br />
                                 <br />
                                 <div className='form-row'>
                                     <label></label>
                                     <div className='btnSalvar'>
-                                        <button>SALVAR</button>    
+                                        <button onClick={salvarClick}>SALVAR</button>    
                                     </div>
                                 </div>
                             </div>
